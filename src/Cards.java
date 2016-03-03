9f12ec556ec8dc1774d1c2ff09ab69252be5b948
golmardj <golmardj@PC-A213-03.iut-valence.fr>
2016-03-03 12:01:01 +0100

    Javadoc was created for MemoryGame the class cards was created for the tab grids.

---
+++ src/Cards.java
@@ -0,0 +1,9 @@
+
+/**
+ * @author golmardj
+ *Cards is the class which represents the cards placed on the grid from MemoryGame.
+ */
+public class Cards
+{
+
+}
--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -1,8 +1,33 @@
-
+/**
+ *This class represents a Memory game.
+ * MemoryGame is a game from Memory which is ready to play, the cards was placed on a grid,
+ * on the hidden side. The player has a timer that start from 0 and stop 
+ * when the player win the game.
+ * The player can flip 2 cards on the grid, on the other side.
+ * If they are similar, these cards stay on the visible side. 
+ * 
+ * @author golmardj
+ *
+ */
 public class MemoryGame
 {
-
-	public void startGame()
+	/**
+	 * the attributes grids represents the grid where the cards are placed.
+	 */
+	public Cards[] grids= null;
+	
+	
+	/**
+	 * Creates a new Memory game, ready to be played (explain the attributes)
+	 */
+	public MemoryGame()
+	{
+		
+	}
+	/**
+	 * playGame is a method which represents a game being played by the user.
+	 */
+	public void playGame()
 	{
 		// TODO Auto-generated method stub
 		
